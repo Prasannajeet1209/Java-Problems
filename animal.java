9feb5ca27a740273478b8137c3a06ea05d51364d
Prasannajeet Jain <99951062+Prasannajeet1209@users.noreply.github.com>
2024-06-26 10:20:59 +0530

    Add files via upload

---
+++ animal.java
@@ -0,0 +1,13 @@
+package interface1;
+
+public class animal implements dog{
+public void sound()
+ {
+	System.out.println("I am from cat methods");
+	
+ }
+  public void doing()
+  {
+	  System.out.println("I am from dogs methods");  
+  }
+}
---
+++ dog.java
@@ -0,0 +1,6 @@
+package interface1;
+
+public interface dog extends cat
+{
+ void doing();
+}
