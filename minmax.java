769dfc5a3044499375b1f45781aa9020f714f887
Prasannajeet Jain <99951062+Prasannajeet1209@users.noreply.github.com>
2024-07-07 18:04:42 +0530

    Add files via upload

---
+++ minmax.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
+
+
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+        System.out.println("Reversed Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+        sc.close();
+    }
+}
