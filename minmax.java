825c3c1f58e9f528376562108ced07c34de5c632
Prasannajeet Jain <99951062+Prasannajeet1209@users.noreply.github.com>
2024-08-22 18:10:35 +0530

    Add files via upload

--- minmax.java
+++ minmax.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MinMax {
+public class minmax {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements:");
