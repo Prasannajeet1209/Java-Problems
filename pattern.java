89295f4522393510bbf15590e0120a3dd8cc43a4
Prasannajeet Jain <99951062+Prasannajeet1209@users.noreply.github.com>
2024-08-23 22:21:27 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/**
+ * pattern
+ */
+public class pattern {
+
+    public static void main(String[] args) {
+        for(int i=1 ;i<=4;i++){
+            System.out.println("****");
+        }
+    }
+}
